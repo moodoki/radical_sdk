e3123e6b96e34b97025a35adfb4d3daf296c81e2
moodoki <teckyian@gmail.com>
2020-12-06 17:26:35 +0800

    add v1 calibration constants

---
+++ radicalsdk/radar/v1_constants.py
@@ -0,0 +1,5 @@
+import os
+import numpy as np
+
+F = np.load(os.path.join(os.path.dirname(__file__), 'v1_data/f_matrix.npy'))
+P = np.load(os.path.join(os.path.dirname(__file__), 'v1_data/p_matrix.npy'))
