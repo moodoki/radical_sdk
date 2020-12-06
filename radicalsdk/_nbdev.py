9980593f44f3ac519ff44398daa8504ea11ab286
moodoki <teckyian@gmail.com>
2020-12-06 16:40:26 +0800

    stubs and config reader

--- radicalsdk/_nbdev.py
+++ radicalsdk/_nbdev.py
@@ -3,11 +3,21 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"cfar_processing": "01_radardsp.ipynb",
-         "PolarToCartesianWarp": "03_geometry.ipynb"}
+         "PolarToCartesianWarp": "03_geometry.ipynb",
+         "read_radar_params": "04_radarcfg_v1.ipynb",
+         "cfg_list_to_dict": "04_radarcfg_v1.ipynb",
+         "dict_to_list": "04_radarcfg_v1.ipynb",
+         "channelStr_to_dict": "04_radarcfg_v1.ipynb",
+         "profileStr_to_dict": "04_radarcfg_v1.ipynb",
+         "chirp_to_dict": "04_radarcfg_v1.ipynb",
+         "power_to_dict": "04_radarcfg_v1.ipynb",
+         "frameStr_to_dict": "04_radarcfg_v1.ipynb",
+         "adcStr_to_dict": "04_radarcfg_v1.ipynb"}
 
 modules = ["radardsp.py",
            "h5dataset.py",
-           "geometry.py"]
+           "geometry.py",
+           "radar/config_v1.py"]
 
 doc_url = "https://moodoki.github.io/radicalsdk/"
 
