10afca259580c9991f8bc3fff16fde19c25d9836
moodoki <teckyian@gmail.com>
2020-12-13 13:21:47 +0800

    stub coordinate transform functions

--- radicalsdk/radar/v1_constants.py
+++ radicalsdk/radar/v1_constants.py
@@ -1,10 +1,10 @@
 import os
 import numpy as np
 
-class TupperwearMount_0:
+class TupperwearD435_0:
     F = np.load(os.path.join(os.path.dirname(__file__), 'v1_data/f_matrix.npy'))
     P = np.load(os.path.join(os.path.dirname(__file__), 'v1_data/p_matrix_original.npy'))
 
-class TupperwearMount:
+class TupperwearD435:
     F = np.load(os.path.join(os.path.dirname(__file__), 'v1_data/f_matrix.npy'))
     P = np.load(os.path.join(os.path.dirname(__file__), 'v1_data/p_matrix.npy'))
