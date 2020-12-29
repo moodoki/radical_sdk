a11299cf2304363f77cab9959217cb9b1a176723
moodoki <teckyian@gmail.com>
2020-12-29 18:23:50 +0800

    package data typo

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setuptools.setup(
     url = cfg['git_url'],
     packages = setuptools.find_packages(),
     include_package_data = True,
-    package_data = ['radicalsdk':['*.npy']],
+    package_data = {'radicalsdk':['*.npy']},
     install_requires = requirements,
     dependency_links = cfg.get('dep_links','').split(),
     python_requires  = '>=' + cfg['min_python'],
