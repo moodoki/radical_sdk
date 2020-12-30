00db60b64cde1f65c18ff346112132e215098286
moodoki <teckyian@gmail.com>
2020-12-30 08:47:20 +0800

    rename cfar_nms

--- radicalsdk/_nbdev.py
+++ radicalsdk/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"range_azimuth_ca_cfar": "01_radardsp.ipynb",
+index = {"cfar_nms": "01_radardsp.ipynb",
+         "range_azimuth_ca_cfar": "01_radardsp.ipynb",
          "logger": "04_radarcfg_v1.ipynb",
          "H5DatasetIterator": "02_h5dataset.ipynb",
          "H5DatasetLoader": "02_h5dataset.ipynb",
